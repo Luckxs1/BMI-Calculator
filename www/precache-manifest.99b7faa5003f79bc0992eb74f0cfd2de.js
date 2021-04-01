eca87b8d17d462f3832663f36351e1def5f54abf
Luckxs1 <yunquelucky@Gmail.com>
2021-04-01 18:00:35 +0800

    new existing file

---
+++ www/precache-manifest.99b7faa5003f79bc0992eb74f0cfd2de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb6f89d889737edcc22ee40a9ffb7a8e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "46f776de35d7dab5deb1",
+    "url": "./static/css/2.4a533915.chunk.css"
+  },
+  {
+    "revision": "3fda30fb7a5e82dd8d3d",
+    "url": "./static/css/main.bd9f8a8a.chunk.css"
+  },
+  {
+    "revision": "46f776de35d7dab5deb1",
+    "url": "./static/js/2.f564ab6d.chunk.js"
+  },
+  {
+    "revision": "3fda30fb7a5e82dd8d3d",
+    "url": "./static/js/main.41d71d30.chunk.js"
+  },
+  {
+    "revision": "56413b85a9a22a05ddf8",
+    "url": "./static/js/runtime~main.c00d21a1.js"
+  }
+]);
